285412ea4cd75589c84251bd4ae8b9fef92f2617
Gal <birkagal@gmail.com>
2021-11-25 18:56:32 +0200

    Uploading Reactive Catalog (assignment 2) template

---
+++ reactive-catalog-service/src/test/java/com/birkagal/catalog/ReactiveCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birkagal.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveCatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
